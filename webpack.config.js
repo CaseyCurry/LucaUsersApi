484b5221e14330298d49233082153681798d2e25
Casey Curry <casey.de.curry@gmail.com>
2017-02-13 21:49:06 -0600

    include dependencies in dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const nodeExternals = require("webpack-node-externals");
 const path = require("path");
 
 module.exports = [{
@@ -12,7 +11,6 @@ module.exports = [{
     path: path.resolve(__dirname, "dist"),
     filename: "host.js"
   },
-  externals: [nodeExternals()],
   module: {
     rules: [{
       enforce: "pre",
