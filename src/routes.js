66e3450a958e0030572a50e196cd6e9da5b3773e
CaseyCurry <casey.de.curry@gmail.com>
2017-09-09 12:24:28 -0500

    fix route registration

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,12 @@ const wrap = require("express-async-wrap");
 const authenticateUser = require("./route-handlers/authenticate-user");
 const registerUser = require("./route-handlers/register-user");
 
-module.exports.register = (dependencies) => {
-  return (app) => {
-    app.post("/api/users/:email", wrap(async(request, response) => {
-      await authenticateUser(request, response, dependencies);
-    }));
+module.exports.register = (dependencies, app) => {
+  app.post("/api/users/:email", wrap(async(request, response) => {
+    await authenticateUser(request, response, dependencies);
+  }));
 
-    app.post("/api/users", wrap(async(request, response) => {
-      await registerUser(request, response, dependencies);
-    }));
-  };
+  app.post("/api/users", wrap(async(request, response) => {
+    await registerUser(request, response, dependencies);
+  }));
 };
