ce91d5ec3fb98e2ed6e38384e5ad6f1cf87237ae
CaseyCurry <casey.de.curry@gmail.com>
2019-02-24 17:03:45 -0600

    add method to prevent duplicate users

--- src/infrastructure/aws/dynamoDb/user-repository.js
+++ src/infrastructure/aws/dynamoDb/user-repository.js
@@ -13,6 +13,21 @@ const userRepository = {
       })
       .promise();
   },
+  getById: id => {
+    return client
+      .query({
+        TableName: table,
+        KeyConditionExpression: "id = :id",
+        ExpressionAttributeValues: { ":id": id }
+      })
+      .promise()
+      .then(data => {
+        if (data.Count) {
+          const user = new User(data.Items[0]);
+          return user;
+        }
+      });
+  },
   getByEmail: email => {
     return client
       .query({
